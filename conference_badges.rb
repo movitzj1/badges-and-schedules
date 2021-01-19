e7ae1adae50bc51041a3eb73c3e9981f43e3d35b
Justin Movitz <movitzj1@montclair.edu>
2021-01-19 13:07:35 -0500

    commit tree 82ad55cfaf617f5404280a8060477c291d0ee4df

--- conference_badges.rb
+++ conference_badges.rb
@@ -1,1 +1,28 @@
-# Write your code here.
+def badge_maker(name)
+    "Hello, my name is #{name}."
+end
+
+def batch_badge_creator(names)
+    array = []
+    names.each do |name|
+        phrase = "Hello, my name is #{name}."
+        array << phrase
+    end
+    array
+end
+
+def assign_rooms(names)
+    names.map.with_index(1) do |name, num|
+        "Hello, #{name}! You'll be assigned to room #{num}!"
+    end
+end
+
+def printer(names)
+    batch_badge_creator(names).each do |badge|
+        puts badge
+    end
+    
+    assign_rooms(names).each do |room|
+        puts room
+    end
+end
